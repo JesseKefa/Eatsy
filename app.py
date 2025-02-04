ae55dc2e1fdcfa26e28a12152b0303ab282e913b
Jesse <jessekefa86@gmail.com>
2025-02-04 14:23:34 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    msg = request.form.get('Body', 'Hello, World!')
+    response = {
+        "messages": [
+            {"text": f"Echo: {msg}"}
+        ]
+    }
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
